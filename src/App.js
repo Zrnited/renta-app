bdba805184b88b4aa6b33fb6d375b7d42aac1ec7
Zrnited <kolamayh@gmail.com>
2023-03-11 15:45:16 +0100

    Installed a React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl bg-red-700 text-white font-extrabold underline">
+        RENTA
+      </h1>
+    </div>
+  );
+}
+
+export default App;
